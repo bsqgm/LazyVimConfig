7abce443cc93dfccded8a860d58258687c3f461a
bsqgm <37140581+bsqgm@users.noreply.github.com>
2024-05-27 14:19:44 +0800

    浅色、IME、换行

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,42 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+--
+vim.opt.background = "light"
+
+local opt = vim.opt
+opt.wrap = true
+
+local function set_ime(args)
+  if args.event:match("Enter$") then
+    vim.g.neovide_input_ime = true
+  else
+    vim.g.neovide_input_ime = false
+  end
+end
+
+local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+  group = ime_input,
+  pattern = "*",
+  callback = set_ime,
+})
+
+vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+  group = ime_input,
+  pattern = "[/\\?]",
+  callback = set_ime,
+})
+
+--vim.g.neovide_cursor_vfx_mode = "railgun"
+
+local group_cdpwd = vim.api.nvim_create_augroup("group_cdpwd", { clear = true })
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = group_cdpwd,
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_current_dir(vim.fn.expand("%:p:h"))
+  end,
+})
