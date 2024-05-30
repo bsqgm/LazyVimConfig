3059b0ba4094a248426202e616149c67d8d28858
bsqgm <37140581+bsqgm@users.noreply.github.com>
2024-05-31 02:14:45 +0800

    修改调试按键映射

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,13 @@
 local map = LazyVim.safe_keymap_set
 
 map("n", "<leader>cj", "<cmd>ClangdSwitchSourceHeader<cr>", { desc = "Switch Source Header" })
+
+-- dap
+map("n", "<leader>dT", "<cmd>lua require('dapui').float_element('stacks', {})<cr>", { noremap = true, silent = true , desc = "Stacks"})
+map("n", "<F9>", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", { noremap = true, silent = true , desc = "Toggle Breakpoint"})
+map("n", "<F5>", "<cmd>lua require'dap'.continue()<cr>", opts)
+map("n", "<F10>", "<cmd>lua require'dap'.step_over()<cr>", opts)
+map("n", "<F11>", "<cmd>lua require'dap'.step_into()<cr>", opts)
+map("n", "<s-F11>", "<cmd>lua require'dap'.step_out()<cr>", opts)
+map("n", "<s-F5>", "<cmd>lua require'dap'.terminate()<cr>", opts)
+map("n", "<F6>", "<cmd>lua require'dap'.run_last()<cr>", opts)
